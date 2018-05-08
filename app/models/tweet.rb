200d1e5d600a608f68c530e49cccf74df34a09f3
Javi Filella <javi.filella@sage.com>
2018-05-08 09:03:38 +0200

    Add Time (formated) & Add Gravatar

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,10 +2,15 @@ class Tweet < ActiveRecord::Base
   belongs_to :user
 
   def as_json(_options = {})
-    super(methods: [:name])
+    super(methods: [:name, :gravatar])
   end
 
   def name
     user.display_name
+  end
+
+  def gravatar
+    hash = Digest::MD5.hexdigest(user.email)
+    "http://www.gravatar.com/avatar/#{hash}"
   end
 end
